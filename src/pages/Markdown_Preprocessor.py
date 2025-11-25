90bb9bd0dc1d7178d868e93860da5015cd70c5ca
patrickab <patrick.a.bitzer@gmail.com>
2025-11-25 20:01:33 +0100

    code-quality(md-preprocessor): Simplify logic / improve UI

--- src/pages/Markdown_Preprocessor.py
+++ src/pages/Markdown_Preprocessor.py
@@ -1,41 +1,54 @@
 import os
+import subprocess
 
 import streamlit as st
 
-from src.config import DIRECTORY_VLM_OUTPUT
+from src.config import DIRECTORY_RAG_INPUT, DIRECTORY_VLM_OUTPUT
 from src.lib.streamlit_helper import editor
 
 
 def markdown_preprocessor() -> None:
-    """
-    Markdown Preprocessor for Obsidian Notes.
-    """
-    st.title("Markdown Preprocessor")
-    vlm_output = os.listdir(DIRECTORY_VLM_OUTPUT)
-    vlm_output = [d.split("converted_")[1] for d in vlm_output]
-    vlm_output = [d.split(".pdf")[0] for d in vlm_output]
-    for output in vlm_output:
-        with st.expander(output):
+    """Markdown Preprocessor for Obsidian Notes."""
+    _,center, _ = st.columns([1,8,1])
+    with center:
+        st.title("Markdown Preprocessor")
+
+        vlm_output = os.listdir(DIRECTORY_VLM_OUTPUT)
+        vlm_output = [d.split("converted_")[1] for d in vlm_output]
+        vlm_output = [d.split(".pdf")[0] for d in vlm_output]
+
+        for output in vlm_output:
+
             content_path = f"./{DIRECTORY_VLM_OUTPUT}/converted_{output}.pdf/{output}/auto"
             contents = os.listdir(content_path)
-            md_file = next(f for f in contents if f.endswith(".md"))
-
+            md_filepath = next(f for f in contents if f.endswith(".md"))
             imgs_path = content_path + "/images"
-            imgs = os.listdir(imgs_path)
+
+            with st.expander(output):
+
+                cols_spacer = st.columns([0.1,0.9])
+
+                imgs = os.listdir(imgs_path)
     
-            imsgs_paths = [f"{imgs_path}/{img}" for img in imgs]
-            md_path = f"{content_path}/{md_file}"
+                imsgs_paths = [f"{imgs_path}/{img}" for img in imgs]
+                md_path = f"{content_path}/{md_filepath}"
 
-            with open(md_path, "r") as f:
-                md_content = f.read()
+                with open(md_path, "r") as f:
+                    md_content = f.read()
 
-            cols = st.columns(2)
+                cols = st.columns(2)
 
-            with cols[0]:
-                edited_text = editor(language="latex", text_to_edit=md_content, key=output)
-                edited_text
-            with cols[1]:
-                st.markdown(edited_text)
+                with cols[0]:
+                    edited_text = editor(language="latex", text_to_edit=md_content, key=output)
+                with cols[1]:
+                    st.markdown(edited_text)
+
+                with cols_spacer[0]:
+                    if st.button("Save Changes", key=f"save_md_{output}"):
+                        with open(md_path, "w") as f:
+                            f.write(edited_text)
+                        st.success(f"Saved changes to {md_filepath}")
+
 
 if __name__ == "__main__":
     markdown_preprocessor()
