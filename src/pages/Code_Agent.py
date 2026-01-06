afa39a7e206923a1992b295ddc739a1e2b4850d3
Your Name <you@example.com>
2026-01-06 19:27:21 +0100

    fix(agents-in-a-box): update to new package structure

--- src/pages/Code_Agent.py
+++ src/pages/Code_Agent.py
@@ -1,7 +1,7 @@
-from code_agents.agents import agent_controls, chat_interface
+from code_agents.app_ui import agent_controls, chat_interface, controller
 import streamlit as st
 
 if __name__ == "__main__":
     with st.sidebar:
-        agent_controls()
-    chat_interface()
+        agent_controls(controller=controller)
+    chat_interface(controller=controller)
